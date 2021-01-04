f53c073f297b2646704d407d13a4359c486b9e1b
k1mk8 <karmatkasperek@gmail.com>
2021-01-04 15:06:01 +0100

    move function in board

--- player.py
+++ player.py
@@ -17,7 +17,66 @@ class Player:
     def color(self):
         return self._color
 
-    def move_on_board(self):
-        print(f'Teraz ruch gracza: {self.name()}')
+    def move_on_board(self, table):
+        print(f'Teraz ruch gracza: {self.name()}, jeśli nie ma prawdiłowego ruchu wpisz dwa zera')
         row = input("Podaj numer wiersza:\n")
         column = input("Podaj numer kolumny:\n")
+        if int(row) == 0 and int(column) == 0:
+            pass
+        else:
+            if self.color() == "Black":
+                self.inrow(table, row, column, "Black")
+                self.incolumn(table, row, column, "Black")
+            elif self.color() == "White":
+                self.inrow(table, row, column, "White")
+                self.incolumn(table, row, column, "White")
+
+    def inrow(self, table, row, column, name=""):
+        if name == "Black":
+            letter = "C "
+        elif name == "White":
+            letter = "B "
+        a = int(row)
+        b = int(column) + 1
+        while table[a][b] != "0 " and b < len(table):
+            if table[a][b] == f'{letter}':
+                break
+            b += 1
+        for i in range(int(column), b):
+            table[a][i] = f'{letter}'
+        a = int(row)
+        b = int(column) - 1
+        while table[a][b] != "0 " and b > 0:
+            if table[a][b] == f'{letter}':
+                break
+            b -= 1
+        for i in range(b + 1, int(column) + 1):
+            table[a][i] = f'{letter}'
+
+    def incolumn(self, table, row, column, name=""):
+        if name == "Black":
+            letter = "C "
+        elif name == "White":
+            letter = "B "
+        a = int(row) + 1
+        b = int(column)
+        while table[a][b] != "0 " and a < len(table):
+            if table[a][b] == f'{letter}':
+                break
+            a += 1
+        for i in range(int(column), a):
+            table[i][b] = f'{letter}'
+        a = int(row) - 1
+        b = int(column)
+        while table[a][b] != "0 " and a > 0:
+            if table[a][b] == f'{letter}':
+                break
+            a -= 1
+        for i in range(a + 1, int(column) + 1):
+            table[a][i] = f'{letter}'
+
+    def diagonally(self, table, row, column, name=""):
+        if name == "Black":
+            letter = "C "
+        elif name == "White":
+            letter = "B "
