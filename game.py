4a48131dffca3d590f1a21cfe64dc8fc63f14b21
k1mk8 <karmatkasperek@gmail.com>
2021-01-11 02:38:37 +0100

    count points and end game

--- game.py
+++ game.py
@@ -15,6 +15,12 @@ class Game():
     def player2(self):
         return self._player2
 
+    def computer1(self):
+        return self._computer1
+
+    def computer2(self):
+        return self._computer2
+
     def interface(self):
         return self._interface
 
@@ -32,14 +38,31 @@ class Game():
         self._player2 = Player("White")
         possible_moves = self.interface().board.possible_moves()
         total_points = self.player1().points() + self.player2().points()
-        while total_points < possible_moves:
-            self.player1().move_on_board()
-            self.player2().move_on_board()
+        while total_points < possible_moves or self.player1.points() == 0 or self.player2.points() == 0:
+            self.player1().move_on_board(self.interface().board.table_board)
+            self.player2().count_points(self.interface().board.table_board)
+            self.interface().board.draw_board()
+            self.player2().move_on_board(self.interface().board.table_board)
+            self.player1().count_points(self.interface().board.table_board)
+            self.interface().board.draw_board()
+        if self.player1.points() > self.player2.points():
+            print(f"Gratulacje! Wygrał gracz: {self.player1.name()}")
+        elif self.player1.points() < self.player2.points():
+            print(f"Gratulacje! Wygrał gracz: {self.player2.name()}")
+        elif self.player1.points() == self.player2.points():
+            print("Remis!")
+        self.interface().after_game()
 
     def oneVSComputer(self):
         self._player1 = Player("Black")
         self._computer1 = Computer("White")
-        pass
+        if self.player1.points() > self.computer1.points():
+            print(f"Gratulacje! Wygrał gracz: {self.player1.name()}")
+        elif self.player1.points() < self.computer1.points():
+            print(f"Gratulacje! Wygrał gracz: {self.computer1.name()}")
+        elif self.player1.points() == self.computer1.points():
+            print("Remis!")
+        self.interface().after_game()
 
     def ComputerVsComputer(self):
         self._computer1 = Computer("Black")
