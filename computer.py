446b41089c30e247d1b82a9d2bf35733e9171f26
k1mk8 <karmatkasperek@gmail.com>
2021-01-11 02:44:01 +0100

    start computer

--- computer.py
+++ computer.py
@@ -1,3 +1,27 @@
 class Computer:
-    def __init__(self, color=""):
+    def __init__(self, name, color):
+        self._name = name
+        self._points = 2
+        self._color = color
+
+    def name(self):
+        return self._name
+
+    def points(self):
+        return self._points
+
+    def color(self):
+        return self._color
+
+    def set_points(self, points):
+        self._points = points
+
+    def letter(self, name=""):
+        if name == "Black":
+            letter = "C "
+        elif name == "White":
+            letter = "B "
+        return letter
+
+    def move_on_board(self, table):
         pass
