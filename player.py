06ccdedbbb6fdf7d1b7538baff6118a6abba64a5
k1mk8 <karmatkasperek@gmail.com>
2021-01-11 02:39:05 +0100

    diagonally and count points

--- player.py
+++ player.py
@@ -24,14 +24,10 @@ class Player:
         if int(row) == 0 and int(column) == 0:
             pass
         else:
-            if self.color() == "Black":
-                self.inrow(table, row, column, "Black")
-                self.incolumn(table, row, column, "Black")
-                self.diagonally(table, row, column, "Black")
-            elif self.color() == "White":
-                self.inrow(table, row, column, "White")
-                self.incolumn(table, row, column, "White")
-                self.diagonally(table, row, column, "White")
+            self.inrow(table, row, column, self.color())
+            self.incolumn(table, row, column, self.color())
+            self.diagonally(table, row, column, self.color())
+            self.count_points(table, row, column)
 
     def letter(self, name=""):
         if name == "Black":
@@ -42,72 +38,85 @@ class Player:
 
     def inrow(self, table, row, column, name=""):
         letter = self.letter(name)
+        table[int(row)][int(column)] = letter
         a = int(row)
         b = int(column) + 1
-        while table[a][b] != "0 " and b < len(table):
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and b < len(table[0]):
+            if table[a][b] == "0 ":
+                b = int(column) + 1
                 break
             b += 1
         for i in range(int(column), b):
             table[a][i] = f'{letter}'
         a = int(row)
         b = int(column) - 1
-        while table[a][b] != "0 " and b > 0:
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and b > 0:
+            if table[a][b] == "0 ":
+                b = int(column) - 1
                 break
             b -= 1
-        for i in range(b + 1, int(column) + 1):
+        b += 1
+        for i in range(b, int(column)):
             table[a][i] = f'{letter}'
 
     def incolumn(self, table, row, column, name=""):
         letter = self.letter(name)
         a = int(row) + 1
         b = int(column)
-        while table[a][b] != "0 " and a < len(table):
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and a < len(table):
+            if table[a][b] == "0 ":
+                a = int(row) + 1
                 break
             a += 1
         for i in range(int(row), a):
             table[i][b] = f'{letter}'
         a = int(row) - 1
         b = int(column)
-        while table[a][b] != "0 " and a > 0:
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and a > 0:
+            if table[a][b] == "0 ":
+                a = int(row) - 1
                 break
             a -= 1
-        for i in range(a + 1, int(row) + 1):
+        a += 1
+        for i in range(a, int(row)):
             table[i][b] = f'{letter}'
 
     def diagonally(self, table, row, column, name=""):
         letter = self.letter(name)
         a = int(row) - 1
         b = int(column) - 1
-        while table[a][b] != "0 " and a > 0 and b > 0:
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and a > 0 and b > 0:
+            if table[a][b] == "0 ":
+                a = int(row) - 1
+                b = int(row) - 1
                 break
             a -= 1
             b -= 1
         a += 1
         b += 1
-        for i in range(a, int(row) + 1):
+        for i in range(a, int(row)):
             table[i][b] = f'{letter}'
             b += 1
         a = int(row) - 1
         b = int(column) + 1
-        while table[a][b] != "0 " and a > 0 and b < len(table):
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and a > 0 and b < len(table[0]):
+            if table[a][b] == "0 ":
+                a = int(row) - 1
+                b = int(column) + 1
                 break
             a -= 1
             b += 1
         a += 1
         b -= 1
-        for i in range(a, int(row) + 1):
+        for i in range(a, int(row)):
             table[i][b] = f'{letter}'
             b -= 1
         a = int(row) - 1
         b = int(column) - 1
-        while table[a][b] != "0 " and a < len(table) and b < len(table):
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and a < len(table) and b < len(table[0]):
+            if table[a][b] == "0 ":
+                a = int(row) - 1
+                b = int(column) - 1
                 break
             a += 1
             b += 1
@@ -118,13 +127,33 @@ class Player:
             b += 1
         a = int(row) + 1
         b = int(column) - 1
-        while table[a][b] != "0 " and a < len(table) and b > 0:
-            if table[a][b] == f'{letter}':
+        while table[a][b] != f'{letter}' and a < len(table) and b > 0:
+            if table[a][b] == "0 ":
+                a = int(row) + 1
+                b = int(column) - 1
                 break
             a += 1
             b -= 1
         a -= 1
         b += 1
-        for i in range(int(row), a):
+        for i in range((int(row) + 1), (a + 1)):
             table[i][b] = f'{letter}'
             b += 1
+
+    def count_points(self, table, row=0, column=0):
+        letter = self.letter(self.color())
+        number_of_appearances = 0
+        for i in range(1, (len(table))):
+            for j in range(1, (len(table[0]))):
+                if table[i][j] == letter:
+                    number_of_appearances += 1
+        if number_of_appearances > self.points() + 1:
+            self.set_points(number_of_appearances)
+        else:
+            if int(row) == 0 and int(column) == 0:
+                self.set_points(number_of_appearances)
+                pass
+            else:
+                print("Nieprawidłowy ruch! Proszę podać inne pola.")
+                table[int(row)][int(column)] = "0 "
+                self.move_on_board(table)
